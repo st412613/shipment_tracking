7c3d967bf99da003b35421b4bf43069094d50e52
ShiRitika <shiritika15031999@gmail.com>
2023-12-02 12:43:52 +0530

    added some data

--- src/components/trackShipment/ShipmentTrackStyles.js
+++ src/components/trackShipment/ShipmentTrackStyles.js
@@ -43,6 +43,113 @@ const ShipmentTrackStyles = makeStyles(() => ({
       }
     }
   },
+  responseContainer: {
+    margin: '10px auto',
+  },
+  trackIDContainer: {
+    paddingBottom: '8px',
+    margin: '10px 0px',
+    borderBottom: '1px solid #bbd7cb',
+    '& h3': {
+      fontSize: '20px',
+      '& span': {
+        fontWeight: '400'
+      },
+    },
+  },
+  summaryContainer: {
+    backgroundColor: '#fff',
+    width: '60%',
+    padding: '1rem',
+    margin: '10px 0px',
+    '& h4': {
+      borderBottom: '2px solid #ecf4f9',
+      paddingBottom: '10px',
+      fontSize: '20px',
+    },
+    '& h1': {
+      '& span': {
+        fontSize: '20px',
+      }
+    }
+  },
+  upperContent: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  statusContent: {
+    '& h5' :{
+      backgroundColor: '#c8fad9',
+      color:'#018b71',
+      borderRadius: '3px',
+      fontSize: '14px',
+      padding: '8px 10px',
+      fontWeight: '500',
+      display: 'inline-block',
+    },
+  },
+  summaryContent: {
+    '& h5': {
+      fontSize: '24px',
+      fontWeight: '400'
+    },
+  },
+  packTypContent: {
+    backgroundColor: '#dbf0e6',
+    borderRadius: '6px',
+    display: 'inline-block',
+    color: '#5b80f7',
+    fontSize: '14px',
+    padding: '2px 10px',
+    fontWeight: '500'
+  },
+  trackDetailsContent: {
+    margin: '10px 0px 0px 0px',
+    position: 'relative'
+  },
+  trackingStepper: {
+    padding: '4px 6px',
+  },
+  greenBox: {
+    width: '20px',
+    height: '20px',
+    backgroundColor: '#0d9d59',
+    position: 'absolute',
+    left: -2,
+    top: 0,
+  },
+  cardStepper: {
+    padding: '0 0 20px 8px',
+    borderLeft: '2.5px dotted #0d9d59',
+    border: 'none'
+  },
+  cardHeader: {
+    margin: '10px',
+    '& h5': {
+      fontSize: '16px',
+      display: 'flex',
+      alignItems: 'center',
+      '& span': {
+        marginRight: '10px',
+        display: 'flex',
+        alignItems: 'center',
+      },
+    },
+    '& p': {
+      color: '#766969',
+      fontSize: '13px',
+    },
+  },
+  msgContent: {
+    borderRadius: '2px',
+    backgroundColor: '#dbf0e6',
+    margin: '1px auto',
+    width: '90%',
+    padding: '8px',
+    fontSize: '14px',
+    color: '#000000',
+    fontWeight: '500'
+  },
 
   '@media screen and (max-width: 768px)': {
     formContainer:{
--- src/components/trackShipment/ShipmentTracking.jsx
+++ src/components/trackShipment/ShipmentTracking.jsx
@@ -4,22 +4,36 @@ import { Search } from '@mui/icons-material';
 
 const ShipmentTracking = () => {
   const classes = ShipmentTrackStyles();
-  const [pod_Input, setPODInput] = useState('');
+  const [podInput, setPODInput] = useState('');
+  const [responseData, setResponseData] = useState(null);
 
   const handleTextareaChange = (event) => {
     setPODInput(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     // Perform validation here (you can customize the validation logic)
-    if (!pod_Input.trim()) {
+    if (!podInput.trim()) {
       return;
     }
 
-    // Handle the submit logic (e.g., API call, processing data)
-    console.log('Submitted Airway No:', pod_Input);
+    try {
+      // Construct the API URL with the entered airway number
+      const apiUrl = `https://dlv-api.delhivery.com/v3/unified-tracking?wbn=${podInput}`;
+
+      // Make the API request
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      // Handle the response data (you can update state, display data, etc.)
+      console.log('API Response:', data);
+      setResponseData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle errors as needed
+    }
   };
 
   return (
@@ -30,7 +44,7 @@ const ShipmentTracking = () => {
             <input
               type="text"
               placeholder="Airway No."
-              value={pod_Input}
+              value={podInput}
               onChange={handleTextareaChange}
             />
             <button type="submit" className={classes.searchButton}>
@@ -38,6 +52,54 @@ const ShipmentTracking = () => {
             </button>
           </div>
         </form>
+
+        {/* Display the API response data */}
+        {responseData && responseData.data && responseData.data.length > 0 && (
+          <div className={classes.responseContainer}>
+            {/* Display Tracking ID */}
+            <div className={classes.trackIDContainer}>
+              <h3>TRACKING ID <span>{responseData.data[0].awb} (LR Number: {responseData.data[0].lrn})</span></h3>
+            </div>
+
+            {/* Display Tracking Summary */}
+            <div className={classes.summaryContainer}>
+              <div className={classes.upperContent}>
+                <h4>{responseData.data[0].deliveryDateText}</h4>
+                <div className={classes.statusContent}>
+                  <h5>{responseData.data[0].status.status}</h5>
+                </div>
+              </div>
+              <div className={classes.summaryContent}>
+                {/* Extracting only the date portion */}
+                <h5>{responseData.data[0].deliveryDate.split(',')[0]}</h5>
+              </div>
+              <div className={classes.packTypContent}>{responseData.data[0].packageType}</div>
+            </div>
+
+            {/* Display Tracking Details */}
+            <div className={classes.summaryContainer}>
+              <h4>Tracking details</h4>
+              {responseData.data[0].trackingStates.map((state, index) => (
+                <div key={index} className={classes.trackDetailsContent}>
+                  <div className={classes.trackingStepper}>
+                    <div className={classes.greenBox}></div>
+                    <div className={classes.cardStepper}>
+                      <div className={classes.cardHeader}>
+                        <h5><span>{state.label}</span></h5>
+                        <p>{state.date}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              <div className={classes.msgContent}>Your Order was delivered on {responseData.data[0].deliveryDate}</div>
+            </div>
+
+            {/* Display the entire API response for debugging purposes */}
+            <pre>{JSON.stringify(responseData, null, 2)}</pre>
+          </div>
+        )}
       </div>
     </div>
   );
